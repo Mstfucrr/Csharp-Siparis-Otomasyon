498c239fcb4061f7a0430d37693b349419298c76
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-08 01:33:42 +0300

    Add form for Customer login screen

---
+++ CustomerLoginForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace PROJECT
+{
+    partial class CustomerLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtPass = new System.Windows.Forms.TextBox();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtPass
+            // 
+            this.TxtPass.Location = new System.Drawing.Point(147, 147);
+            this.TxtPass.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.TxtPass.Name = "TxtPass";
+            this.TxtPass.Size = new System.Drawing.Size(221, 27);
+            this.TxtPass.TabIndex = 9;
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Location = new System.Drawing.Point(147, 96);
+            this.TxtEmail.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(221, 27);
+            this.TxtEmail.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(46, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Parola";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(46, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Eposta";
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnLogin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BtnLogin.Location = new System.Drawing.Point(157, 230);
+            this.BtnLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(99, 47);
+            this.BtnLogin.TabIndex = 10;
+            this.BtnLogin.Text = "Giriş Yap";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Demi", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(99, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 39);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Müşteri Giriş";
+            // 
+            // CustomerLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(412, 339);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPass);
+            this.Controls.Add(this.TxtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "CustomerLoginForm";
+            this.Text = "Müşteri Girişi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtPass;
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ CustomerLoginForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT
+{
+    public partial class CustomerLoginForm : Form
+    {
+        public CustomerLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            MessageBox.Show(customer.CustomerLogin(TxtEmail.Text, TxtPass.Text)
+                ? "Başarıyla Giriş Yaptınız"
+                : "Eposta veya parola Hatalı");
+        }
+    }
+}
