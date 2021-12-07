49fd60003cb2aae1f19f728c2403f27397c398a6
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-07 17:06:30 +0300

    Add dbo

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Ad] NVARCHAR(50) NOT NULL, 
+    [Soyad] NVARCHAR(50) NOT NULL,
+    [Adres] NVARCHAR(MAX) NOT NULL, 
+    [Created_date] DATETIME NOT NULL
+)
