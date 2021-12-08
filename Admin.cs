17d8fb9b760207ec31c80ff65f4494008dcfa074
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-08 15:16:08 +0300

    Add Login Form for Admin

---
+++ Admin.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace PROJECT
+{
+    class Admin
+    {
+        private string Username { get; set; }
+
+        private string Password { get; set; }
+
+        
+
+        public Admin(string username,string password)
+        {
+
+            this.Username = "admin";
+            this.Password = "admin";
+
+            MessageBox.Show((this.Username == username && this.Password == password)
+                ? "Başarıyla Giriş Yaptınız"
+                : "Kullanıcı adınız veya Parolanız Hatalı !");
+        }
+        
+    }
+}
---
+++ AdminLoginForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PROJECT
+{
+    public partial class AdminLoginForm : Form
+    {
+        public AdminLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Login_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin(TxtUsername.Text,TxtPass.Text);
+        }
+    }
+}
