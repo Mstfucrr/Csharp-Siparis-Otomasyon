047d4f409282abacd4cc375d73eb7fc970cf6294
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-08 18:35:59 +0300

    Add customer Show form

---
+++ CustomerShowForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace PROJECT
+{
+    partial class CustomerShowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CustomerShowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 372);
+            this.Name = "CustomerShowForm";
+            this.Text = "Müşteri Görüntüle";
+            this.Load += new System.EventHandler(this.CustomerShowForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        }
+}
