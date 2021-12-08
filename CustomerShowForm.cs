d84dbcf2bb08a8fd226e5fdcfd819d931e53d9c3
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-08 21:18:27 +0300

    Add data set connection

--- CustomerShowForm.cs
+++ CustomerShowForm.cs
@@ -21,7 +21,6 @@ namespace PROJECT
         {
             // TODO: Bu kod satırı 'customerDataSet.Customers' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
             this.customersTableAdapter.Fill(this.customerDataSet.Customers);
-            MessageBox.Show("Test");
         }
     }
 }
