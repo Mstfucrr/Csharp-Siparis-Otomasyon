ed44b34842229de02c48ad7d4d26fd62ab0a098e
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-09 19:51:26 +0300

    Add Payment and Payment's child class

---
+++ PaymentCash.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace PROJECT
+{
+    class PaymentCash:Payment
+    {
+        public float cashTendered { get => base.Amount; }
+
+        public PaymentCash()
+        {
+            MessageBox.Show("Kargo yolda\nÖdeme tutarı : " + cashTendered);
+        }
+    }
+}
