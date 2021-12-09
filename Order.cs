f18fb454dcd93109ebf1992c36af59ccc43786d5
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-09 19:50:49 +0300

    Add Order , OrderDetail class and OrderForm

---
+++ Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROJECT
+{
+    class Order
+    {
+        public int CustomerId { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+        public string Payment { get; set; }
+
+        public void calcTax()
+        {
+
+        }
+
+        public void calcTotal()
+        {
+
+        }
+        public void calcTotalWeight()
+        {
+
+        }
+
+        
+    }
+}
---
+++ OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿namespace PROJECT
+{
+    class OrderDetail
+    {
+        public int Quantity { get; set; }
+        public bool TaxStatus { get; set; }
+        public int ProductId;
+        public int OrderId;
+
+        public void CalcSubTotal()
+        {
+
+        }
+
+        public void CalcWeight()
+        {
+
+        }
+    }
+}
