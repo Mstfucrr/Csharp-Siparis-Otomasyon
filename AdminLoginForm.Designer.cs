b0e0e0843e231d3c63cd7ecec9435d1cb5bd2fac
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-12 03:01:51 +0300

    Add admin login bg image

--- AdminLoginForm.Designer.cs
+++ AdminLoginForm.Designer.cs
@@ -29,20 +29,23 @@ namespace PROJECT
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminLoginForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.TxtUsername = new System.Windows.Forms.TextBox();
             this.TxtPass = new System.Windows.Forms.TextBox();
             this.Btn_Login = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(159, 24);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(150, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 48);
             this.label1.TabIndex = 0;
@@ -51,9 +54,10 @@ namespace PROJECT
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(22, 122);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(61, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(151, 28);
             this.label2.TabIndex = 1;
@@ -62,9 +66,10 @@ namespace PROJECT
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(92, 183);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(61, 163);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 28);
             this.label3.TabIndex = 2;
@@ -73,17 +78,17 @@ namespace PROJECT
             // TxtUsername
             // 
             this.TxtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.TxtUsername.Location = new System.Drawing.Point(220, 122);
+            this.TxtUsername.Location = new System.Drawing.Point(66, 117);
             this.TxtUsername.Name = "TxtUsername";
-            this.TxtUsername.Size = new System.Drawing.Size(179, 30);
+            this.TxtUsername.Size = new System.Drawing.Size(302, 30);
             this.TxtUsername.TabIndex = 3;
             // 
             // TxtPass
             // 
             this.TxtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.TxtPass.Location = new System.Drawing.Point(220, 182);
+            this.TxtPass.Location = new System.Drawing.Point(66, 194);
             this.TxtPass.Name = "TxtPass";
-            this.TxtPass.Size = new System.Drawing.Size(179, 30);
+            this.TxtPass.Size = new System.Drawing.Size(302, 30);
             this.TxtPass.TabIndex = 4;
             this.TxtPass.UseSystemPasswordChar = true;
             // 
@@ -100,20 +105,35 @@ namespace PROJECT
             this.Btn_Login.UseVisualStyleBackColor = false;
             this.Btn_Login.Click += new System.EventHandler(this.Btn_Login_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 64);
+            this.panel1.TabIndex = 6;
+            // 
             // AdminLoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Crimson;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(441, 318);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.Btn_Login);
             this.Controls.Add(this.TxtPass);
             this.Controls.Add(this.TxtUsername);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "AdminLoginForm";
             this.Text = "Admin Giriş";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,5 +147,6 @@ namespace PROJECT
         private System.Windows.Forms.TextBox TxtUsername;
         private System.Windows.Forms.TextBox TxtPass;
         private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.Panel panel1;
     }
 }
