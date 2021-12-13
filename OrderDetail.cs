66df2bf6bf9f428e18c1875e171b86e3d2a78f9a
Mstfucrr <mustafa19071907mustafa@gmail.com>
2021-12-13 20:46:05 +0300

    Add OrderDetails Db connect and save data to db

--- OrderDetail.cs
+++ OrderDetail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace PROJECT
 {
@@ -11,10 +12,57 @@ namespace PROJECT
         public int ProductId;
         public int OrderId;
         private Product product = new Product();
+        private DB db = new DB();
 
         public OrderDetail(int orderId)
         {
             this.OrderId = orderId;
+
+            SqlDataReader reader = OrderIdCheckFromOrderDetailsDb();
+            if (!reader.Read())
+            {
+                DeleteOrderDetailsFromDb(); //siparişi olmayan sipariş detaylarını siler
+            }
+
+        }
+
+        public OrderDetail()
+        { }
+
+        public void DeleteOrderDetailsFromDb()
+        {
+            string delete = "Delete From OrderDetails where OrderId = @OrderId";
+            SqlCommand cmd = new SqlCommand(delete,db.baglanti());
+            cmd.Parameters.AddWithValue("@OrderId", OrderId);
+            cmd.ExecuteNonQuery();
+            db.baglanti().Close();
+        }
+
+        public void SaveOrderDetailsData()
+        {
+            string sorgu = "Insert into OrderDetails(ProductId, Quantity, TaxStatus, OrderId)" +
+                           "values(@ProductId, @Quantity, @TaxStatus, @OrderId)";
+            SqlCommand cmd = new SqlCommand(sorgu, db.baglanti());
+            cmd.Parameters.AddWithValue("@OrderId", this.OrderId);
+            cmd.Parameters.AddWithValue("@ProductId",this.ProductId);
+            cmd.Parameters.AddWithValue("@Quantity", this.Quantity);
+            cmd.Parameters.AddWithValue("@TaxStatus", TaxStatus);
+            cmd.ExecuteNonQuery();
+
+            db.baglanti().Close();
+        }
+
+
+        private SqlDataReader OrderIdCheckFromOrderDetailsDb()
+        {
+            string check = "Select * From Orders where id = @id";
+            SqlCommand cmd = new SqlCommand(check,db.baglanti());
+            cmd.Parameters.AddWithValue("@id",this.OrderId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            db.baglanti().Close();
+
+            return reader;
+
         }
 
 
