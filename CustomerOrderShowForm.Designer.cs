14a6304b670e5a1d9d01d8525f715bd75accc1b4
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2021-12-16 03:41:14 +0300

    Add details

--- CustomerOrderShowForm.Designer.cs
+++ CustomerOrderShowForm.Designer.cs
@@ -29,11 +29,16 @@ namespace PROJECT
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -43,7 +48,7 @@ namespace PROJECT
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(687, 89);
+            this.panel1.Size = new System.Drawing.Size(1125, 89);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -52,11 +57,11 @@ namespace PROJECT
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.label1.Location = new System.Drawing.Point(259, 19);
+            this.label1.Location = new System.Drawing.Point(382, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(310, 38);
+            this.label1.Size = new System.Drawing.Size(298, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "müşterisinin siparişleri";
+            this.label1.Text = "Müşterinin Siparişleri";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // dataGridView1
@@ -64,19 +69,73 @@ namespace PROJECT
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.DarkGreen;
             this.dataGridView1.Location = new System.Drawing.Point(12, 108);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(655, 289);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Empty;
+            this.dataGridView1.Size = new System.Drawing.Size(686, 400);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.DarkGreen;
+            this.dataGridView2.Location = new System.Drawing.Point(704, 262);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(409, 246);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.LightSteelBlue;
+            this.label2.Location = new System.Drawing.Point(782, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sipariş Detayları";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // CustomerOrderShowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
-            this.ClientSize = new System.Drawing.Size(687, 418);
+            this.ClientSize = new System.Drawing.Size(1125, 531);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "CustomerOrderShowForm";
@@ -85,7 +144,9 @@ namespace PROJECT
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,5 +155,7 @@ namespace PROJECT
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- CustomerOrderShowForm.cs
+++ CustomerOrderShowForm.cs
@@ -16,20 +16,61 @@ namespace PROJECT
         }
 
         private DB db = new DB();
+
+        
+
         private void CustomerOrderShowForm_Load(object sender, System.EventArgs e)
         {
-            string sorgu = "select Orders.id , Orders.TotalAmount , Orders.TotalWeight , Orders.Payment " +
-            "from OrderDetails " +
-            "inner join Orders on OrderDetails.OrderId = Orders.id "+
-            "inner join Products on OrderDetails.ProductId = Products.id "+
-            "inner join Customers on Orders.CustomerId = Customers.id where Customers.id = "+this.CustomerId;
-
-            SqlDataAdapter da = new SqlDataAdapter(sorgu, db.baglanti());
+            string orderQuery = "select Orders.id , Orders.TotalAmount , Orders.TotalWeight , Orders.Payment , Orders.Created_date from Customers inner join Orders " +
+                                "on Orders.CustomerId = Customers.id where Customers.id = " + this.CustomerId;
+            SqlDataAdapter OrderDa = new SqlDataAdapter(orderQuery, db.baglanti());
+            
             DataTable dt = new DataTable();
-            da.Fill(dt);
+            
+            OrderDa.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[1].HeaderText = "Ödeme Tutarı";
+            dataGridView1.Columns[2].HeaderText = "Kargo Ağırlığı";
+            dataGridView1.Columns[3].HeaderText = "Ödeme Yöntemi";
+            dataGridView1.Columns[4].HeaderText = "Sipariş Tarihi";
+
             db.baglanti().Close();
 
-            dataGridView1.DataSource = dt;
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex]; // seçilen satır
+
+                int OrderId = Convert.ToInt32(row.Cells[0].Value);
+
+                if (OrderId == 58)
+                {
+                    MessageBox.Show("Test");
+                }
+
+                string join = "from OrderDetails inner join Orders " +
+                              "on OrderDetails.OrderId = Orders.id inner join Products " +
+                              "on OrderDetails.ProductId = Products.id inner join Customers " +
+                              "on Orders.CustomerId = Customers.id where OrderDetails.OrderId = " + OrderId;
+
+                string detailQueryAd = "select Products.Ad , OrderDetails.Quantity " + join;
+
+                SqlDataAdapter DetaildDa = new SqlDataAdapter(detailQueryAd, db.baglanti());
+                DataTable dt2 = new DataTable();
+                DetaildDa.Fill(dt2);
+
+                dataGridView2.DataSource = dt2;
+
+                dataGridView2.Columns[0].HeaderText = "Ürün Adı";
+                dataGridView2.Columns[1].HeaderText = "Sipariş Miktarı";
+            }
+            catch (Exception)
+            { }
+
         }
     }
 }
